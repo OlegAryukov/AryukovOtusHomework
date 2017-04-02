30b502b5991237e3efcfaebfbb72cb28cbda0dc3
oaryukov <olegaryukov@gmail.com>
2017-04-02 20:12:13 +0300

    Obfuscation branch

--- src/main/java/ru/aryukov/lottery/LotteryMachine.java
+++ src/main/java/ru/aryukov/lottery/LotteryMachine.java
@@ -3,6 +3,7 @@ package ru.aryukov.lottery;
 import java.util.*;
 
 /**
+ * Lottery machine class.
  * Created by tully.
  */
 class LotteryMachine {
@@ -10,11 +11,20 @@ class LotteryMachine {
     private final int winnersCount;
     private long seed = 0;
 
+    /**
+     * Method for variable initializing.
+     * @param emails this emails.
+     * @param winnersCount count of winners.
+     */
     LotteryMachine(List<String> emails, int winnersCount) {
         this.emails = emails;
         this.winnersCount = Math.min(winnersCount, emails.size());
     }
 
+    /**
+     * Method for getting list of winners.
+     * @return List<String> list of winners.
+     */
     List<String> draw() {
         System.out.println("Draw for the seed: " + seed);
         Random rnd = new Random(seed);
@@ -27,11 +37,21 @@ class LotteryMachine {
         return new ArrayList<>(winners);
     }
 
+    /**
+     * Method for setting seed.
+     * @param seed random long seed.
+     * @return seed.
+     */
     LotteryMachine setSeed(long seed) {
         this.seed = seed;
         return this;
     }
 
+    /**
+     * Method for setting seed.
+     * @param str random String seed.
+     * @return seed.
+     */
     LotteryMachine setSeed(String str) {
         this.seed = str.hashCode();
         return this;
---
+++ src/main/java/ru/aryukov/lottery/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package with main class.
+ * Created by oaryukov on 02.04.2017.
+ */
+package ru.aryukov.lottery;
